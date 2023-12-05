6854f05a3f8ed640dabedb966946008889045f63
mathivenkat <138156558+mathivenkatp@users.noreply.github.com>
2023-12-06 01:01:05 +0530

    Update robot_news_station.py

--- src/my_py_pkg/my_py_pkg/robot_news_station.py
+++ src/my_py_pkg/my_py_pkg/robot_news_station.py
@@ -2,11 +2,24 @@
 import rclpy
 from rclpy.node import Node
 
+from example_interfaces.msg import String
+
 class RobotNewsStationNode(Node):
 
     def __init__(self):
         super().__init__("robot_news_station")
-        
+
+        self.robot_name_ = "C3PO"
+        self.publisher_ = self.create_publisher(String,"robot_news",10)
+        self.timer_ = self.create_timer(0.5,self.publish_news)
+        self.get_logger().info("Robot News Station has been started")
+    
+
+    def publish_news(self):
+        msg = String()
+        msg.data = "Hi this is " + str(self.robot_name_) + "from the robot news station"
+        self.publisher_.publish(msg)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -16,4 +29,4 @@ def main(args=None):
 
     
 if __name__ == "__main__":
-    main()
+    main() 
