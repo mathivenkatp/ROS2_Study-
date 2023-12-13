425d6ceff8f1c3bfb43d9eaba87315d61bc7d300
mathivenkatp <mathivenkat.p@gmail.com>
2023-12-14 01:24:07 +0530

    Updated upto section-6 python client

---
+++ ros2_ws/src/my_py_pkg/my_py_pkg/add_two_int_server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+from example_interfaces.srv import AddTwoInts
+
+
+class AddTwoIntServerNode(Node):
+    def __init__(self):
+        super().__init__("add_two_int_server")
+        self.server_ = self.create_service(
+            AddTwoInts, "add_two_ints", self.call_back_add_two_ints
+        )
+        self.get_logger().info("Add Two Int Node has been Started.")
+
+    def call_back_add_two_ints(self, request, response):
+        response.sum = request.a + request.b
+        self.get_logger().info(
+            str(request.a) + " + " + str(request.b) + " = " + str(response.sum)
+        )
+        self.get_logger().info("Inside_callback")
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = AddTwoIntServerNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__": 
+    main()
